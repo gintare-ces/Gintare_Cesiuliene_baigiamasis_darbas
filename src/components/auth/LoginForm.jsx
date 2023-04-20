05dbda9e4af270105f8beec8fb31d3f4662405e1
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-20 21:38:19 +0300

    formik

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,11 +1,21 @@
+import { useFormik } from "formik";
 import React from "react";
 import {Link} from "react-router-dom";
 
 function LoginForm() {
-    
+  const formik =  useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      console.log('values ===', values);
+    }
+  })
+
   return (
     <div className="w-full max-w-xs mt-8 ">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 border hover:border-fuchsia-700">
+      <form onSubmit={formik.handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 border hover:border-fuchsia-700">
         <div className="mb-4">
           <label className="block text-gray-700 text-base font-bold mb-2" htmlFor="email">
             Enter your email
@@ -15,6 +25,9 @@ function LoginForm() {
             id="email"
             type="email"
             placeholder="Email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
         </div>
         <div className="mb-6">
@@ -26,12 +39,16 @@ function LoginForm() {
             id="password"
             type="password"
             placeholder="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
           />
         </div>
         <div className="flex items-center justify-between">
           <button
+            type='submit'
             className="bg-white border-2 border-fuchsia-700 hover:bg-fuchsia-700 text-black hover:text-white font-bold py-2 px-6 rounded-full"
-            type="button">Sign In</button>
+            >Sign In</button>
         <Link to={'/register'} className="inline-block font-bold text-lg text-fuchsia-700 underline">Register here</Link>
         </div>
       </form>
