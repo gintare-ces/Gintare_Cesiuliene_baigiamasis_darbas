b476afbe8af19da46f8603bbf9f4e1abd70edafd
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-21 14:05:06 +0300

    Register form

---
+++ src/components/auth/RegisterForm.jsx
@@ -0,0 +1,83 @@
+import { useFormik } from "formik";
+import React from "react";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
+
+function RegisterForm() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email").required("Email is required"),
+      password: Yup.string()
+        .min(6, "Password must be minimum 6 characters")
+        .required("Password is required"),
+    }),
+    onSubmit: (values) => {
+      console.log("values ===", values);
+    },
+  });
+  return (
+    <div className="w-full max-w-xs mt-8 ">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 border hover:border-fuchsia-700"
+      >
+        <div className="mb-4">
+          <label className="block text-gray-700 text-base font-bold mb-2" htmlFor="email">
+            Enter your email
+          </label>
+          <input
+            className="shadow border rounded w-full py-2 px-3 text-gray-700"
+            id="email"
+            type="email"
+            placeholder="Email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-base font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow border rounded w-full py-2 px-3 text-gray-700"
+            id="password"
+            type="password"
+            placeholder="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div>{formik.errors.password}</div>
+          ) : null}
+        </div>
+        <div className="grid grid-cols-2 gap-6 items-center text-center">
+          <button
+            type="submit"
+            className="bg-white border-2 border-fuchsia-700 hover:bg-fuchsia-700 text-black hover:text-white font-bold py-2 px-6 rounded-full"
+          >
+            Register
+          </button>
+          <p>
+            {" "}
+            Have account?
+            <Link
+              to={"/login"}
+              className="inline-block font-bold text-lg text-fuchsia-700 underline"
+            >
+              Login here
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterForm;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import RegisterForm from '../components/auth/RegisterForm'
 
 function RegisterPage() {
   return (
-    <div className='container mx-auto'>RegisterPage</div>
+    <div className='container mx-auto'>
+      <h1>Register Page</h1>
+      <RegisterForm />
+    </div>
   )
 }
 
