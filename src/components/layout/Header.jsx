a71f5a0431e9e586ea001373ad1eab66a6a80c59
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-22 16:12:46 +0300

    isLoggedIn header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import {Link, NavLink} from "react-router-dom";
+import { useAuthCtx } from '../../store/AuthProvider';
 
 function Header() {
-  const isLoggedIn = false
+  const { isLoggedIn } = useAuthCtx()
   return (
     <header  className='bg-neutral-800 dark:bg-neutral-900'>
         <div className='container mx-auto relative flex w-full flex-wrap items-center justify-between '>
-            <Link to={'/shops'} className='px-2 py-2 text-xl font-roboto text-white  font-bold' ><span className='text-fuchsia-600 text-4xl'>S</span>hop</Link>
+            <Link to={'/'} className='px-2 py-2 text-xl font-roboto text-white  font-bold' ><span className='text-fuchsia-600 text-4xl'>S</span>hop</Link>
             <nav className='pl-6'>
               {isLoggedIn &&
-                <NavLink to={'/shops'} className='px-4 py-4 text-xl font-roboto text-white hover:bg-fuchsia-700 '  >Shops</NavLink>
-              }
-              {isLoggedIn &&
+                <>
+                <NavLink to={'/shops'} className='px-4 py-4 text-xl font-roboto text-white hover:bg-fuchsia-700 '   >Shops</NavLink>
                 <NavLink to={'/shops/add'} className='px-4 py-4 text-xl font-roboto text-white hover:bg-fuchsia-700' >Add Shop</NavLink>
+                </>
               }
               {!isLoggedIn && 
                 <NavLink to={'/login'} className='px-4 py-4 text-xl font-roboto text-white hover:bg-fuchsia-700' >Login</NavLink>
