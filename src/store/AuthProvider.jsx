2ea873747428fbdcfd5148e0e8c982f60b2d13da
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-22 22:03:33 +0300

    logout user

--- src/store/AuthProvider.jsx
+++ src/store/AuthProvider.jsx
@@ -25,6 +25,8 @@ function AuthProvider({ children }) {
         } else {
           // User is signed out
           // ...
+          console.log('Logout user');
+          setUser(null)
           
         }
       });
