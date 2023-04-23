d1c76d0459c162eb0fc0a5270e89774c9c76bdf2
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-24 00:46:54 +0300

    add new shop form

--- src/pages/AddShopPage.jsx
+++ src/pages/AddShopPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import AddShopForm from '../components/shops/AddShopForm'
+import { addDoc, collection } from 'firebase/firestore'
+import { db } from '../firebase/firebase'
+import { toast } from 'react-hot-toast'
 
 function AddShopPage() {
+  async function addNewShop(newShopObj) {
+    try {
+      const docRef = await addDoc(collection(db, 'shops'), newShopObj)
+      console.log('Document written with ID', docRef.id);
+     
+        toast.success('Shop added')
+      }
+    catch(error) {
+        toast.error('Failed')
+        console.log('error ===', error);
+      }
+  }
   return (
     <div className='container mx-auto'>
         <h1>Welcome to Add Shop Page</h1>
+        <AddShopForm onNewShop={addNewShop} />
     </div>
   )
 }
