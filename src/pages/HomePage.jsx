6499174cbc9c550a658709a13f9cd81adfed3ebc
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-24 23:24:18 +0300

    home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+
 function HomePage() {
   return (
-    <div className='container mx-auto'>HomePage</div>
+    <div className='container mx-auto grid md:grid-cols-2 sm:grid-cols-1 sm:mt-8 md:mt-16'>
+      <div className='mt-8'>
+          <h1 className='text-white text-4xl mt-10'>Login to get full accsess to see available shops and promote your own shop</h1>
+
+      </div>
+      <div className='align-bottom '>
+        <img 
+          src="/src/img/—Pngtree—internet seo marketing data login_6379580.png" 
+          alt="Ilustration person marketing and data" 
+          />
+      </div>
+      
+    </div>
   )
 }
 
