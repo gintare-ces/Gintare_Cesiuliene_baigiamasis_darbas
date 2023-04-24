3784c65424d12770ecc7a150c13fdf2248bcda27
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-24 23:25:01 +0300

    app css rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,28 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+
+module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}"
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
-    container: {
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+    extend: {
+      colors: {
+        primary: '#FF0000',
+      },
+      backgroundImage: {
+        'hero': "./src/img/2802373_5149.jpg",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
       },
     },
   },
   plugins: [],
-}
-
+};
