e960744ffc7ffe97ce7c9a19f27ce02988fbb1f7
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-24 22:30:51 +0300

    singl shop card style

--- src/components/shops/SingleShopCard.jsx
+++ src/components/shops/SingleShopCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function SingleShopCard({ item }) {
   return (
-    <div>
-        <img src={item.imageUrl} alt={item.shopName} />
-        <div>
-            <h3>{item.shopName}</h3>
-            <h5>{item.town}</h5>
+    <div className='grid grid-cols-1 md:grid-cols-5 mb-8 shadow-sm bg-slate-100 md:mb-12 md:max-h-28.8 rounded-lg'>
+        <img src={item.imageUrl} alt={item.shopName} className='col-span-2 object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg' />
+        <div className='col-span-3 p-4'>
+            <h3 className='text-2xl font-bold pb-4'>{item.shopName}</h3>
+            <h5 className='text-grey-600 italic'>{item.town}</h5>
             <h6>{item.startYear}</h6>
-            <p>{item.description}</p>
+            <p className='pt-2'>{item.description}</p>
         </div>
     </div>
   )
