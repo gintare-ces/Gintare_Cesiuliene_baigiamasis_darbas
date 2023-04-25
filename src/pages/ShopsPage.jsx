944271b111ef45a5e268df0a69904696b9a145c5
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-25 09:39:47 +0300

    commit

--- src/pages/ShopsPage.jsx
+++ src/pages/ShopsPage.jsx
@@ -1,4 +1,4 @@
-import { collection, doc, getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase/firebase";
 import SingleShopCard from "../components/shops/SingleShopCard";
